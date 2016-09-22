b094224e4321790c5d7d8f907ea095de0744874a
App Academy Student <student@appacademy.io>
2016-09-21 17:59:54 -0700

    Added hand specs. Added hand combination methods

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,7 +1,69 @@
-require 'deck'
+# require 'deck'
+require 'byebug'
+
 
 class Hand
-  def initialize()
-    @draws = [Deck.deal, Deck.deal, Deck.deal, Deck]
+  attr_accessor :draws
+  # CARD_HANDS = {hc: }
+  def initialize(draws = [])
+    @draws = draws
+  end
+
+
+  def exchange(unwanted, new_cards)
+    # debugger
+    unwanted.each do |card|
+      raise "Bad exchange" unless @draws.include?(card)
+    end
+    unwanted.each do |card|
+      @draws.delete(card)
+    end
+
+    new_cards.each do |card|
+      @draws << card
+    end
+  end
+
+
+
+  def straight?
+    counter = sort_draws.first.value
+    sort_draws.each_with_index do |card, index|
+      index +=1
+      next if index >= sort_draws.length
+      return false unless sort_draws[index] == counter + 1
+      counter += 1
+    end
+    true
+  end
+
+  def n_pair
+    pairs = []
+    @draws.each_with_index do |card, idx|
+      @draws.each_with_index do |card2,idx2|
+        next if idx2 <= idx
+        if card == card2
+          pairs << [card, card2]
+        end
+      end
+    end
+
+  end
+
+  def tripple
+    return true if sort_draws.detect{ |e| sort_draws.count(e) == 3 }
+    false
+  end
+
+  def high_card
+    sort_draws.last
+  end
+
+  def sort_draws
+    @draws.sort {|a,b| a.value <=> b.value}.map {|card| card.value }
   end
 end
+
+a = Hand.new
+a.draws = [1,2,3,4,10]
+a.exchange([2,3,4],[5,8,9])
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,12 +1,79 @@
 require 'rspec'
 require 'hand'
+require 'deck'
+require 'byebug'
+
+
 
 describe Hand do
   subject(:hand) {Hand.new}
-
+  let(:deck) {double("deck", :deal => Card.new(8,:spade))}
+  let(:card) {double("card", :value => Card.new())}
+  before(:each) do
+    5.times do
+      hand.draws << deck.deal
+    end
+  end
   describe '#initialize' do
-    it "should have 5 cards" do
+    it "should have zero cards" do
       expect(hand.draws.length).to eq(5)
     end
   end
+
+  describe "#exchange" do
+
+    it "should exchange one card and get one back" do
+      p hand.draws
+      # hand.exchange(, [Card.new(9, :hearts)])
+      expect(hand.draws.length).to eq(5)
+    end
+    it "should exchange two card and get two back" do
+      hand.exchange([deck.deal,deck.deal], [deck.deal,deck.deal])
+      expect(hand.draws.length).to eq(5)
+    end
+    it "should exchange three card and get three back" do
+      hand.exchange([deck.deal,deck.deal,deck.deal], [deck.deal,deck.deal,deck.deal])
+      expect(hand.draws.length).to eq(5)
+    end
+  end
+
+  describe "#straight" do
+    let(:straight_hand) { [ Card.new(1,:spade), Card.new(2,:spade),  Card.new(3,:spade), Card.new(4,:spade), Card.new(5,:spade)] }
+    let(:h4nd) { Hand.new(straight_hand)}
+    it "should return a straight" do
+      expect(h4nd.straight?).to be true
+    end
+  end
+
+  describe "#full_house" do
+    let(:full_hand) { [ Card.new(7,:spade), Card.new(7,:spade),  Card.new(7,:spade), Card.new(12,:spade), Card.new(12,:spade)] }
+    let(:h4nd) { Hand.new(full_hand)}
+    it "should return a full hand" do
+       expect(h4nd.full_house?).to be true
+    end
+  end
+
+  describe "#n_pair" do
+    let(:full_hand) { [ Card.new(7,:spade), Card.new(7,:spade),  Card.new(7,:spade), Card.new(12,:spade), Card.new(12,:spade)] }
+    let(:h4nd) { Hand.new(full_hand)}
+    it "should return a high hand" #do
+      # expect(h4nd.full_hand)
+    #end
+    it "should return a two pair" #do
+      # expect(h4nd.full_hand)
+    #end
+    it "should return a three of a kind" #do
+      # expect(h4nd.full_hand)
+    #end
+  end
+
+  describe "#tripple" do
+    let(:tripple_hand) { [ Card.new(7,:spade), Card.new(7,:spade),  Card.new(7,:spade), Card.new(12,:spade), Card.new(12,:spade)] }
+    let(:h4nd) { Hand.new(tripple_hand)}
+    it "should return true if a tripple is present" do
+      expect(h4nd.tripple).to be true
+    end
+  end
+
+
 end
